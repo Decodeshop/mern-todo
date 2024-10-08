1a621963c3e942beca6715fbce4767ab5e4b8c1e
Decodeshop <deecodeshop@gmail.com>
2024-10-08 20:35:26 +0500

    Your commit message describing the changes

--- server/server.js
+++ server/server.js
@@ -6,7 +6,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors()); // Enable CORS
+app.use(cors(
+    {
+        origin: ["https://deploy-1whq.vercel.app"],
+        methods: ["POST" , "GET"],
+        credentials: true
+    }
+)); // Enable CORS
 app.use(express.json()); // For parsing application/json
 
 mongoose.connect('mongodb://localhost:27017/mern-todo')
